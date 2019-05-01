daef10064ae6cc04b2063f5460e71bbeb5f1eeb7
yy <1023767856@qq.com>
2019-05-01 12:38:08 +0800

    add error handler module

--- app/errors/__init__.py
+++ app/errors/__init__.py
@@ -1,13 +1,14 @@
 """
 author songjie
 """
+from flask import render_template
 
 
 def register_error(app):
     @app.errorhandler(404)
     def page_not_found(error):
-        return 'This page does not exist', 404
+        return render_template('error/error.html', code='404'), 404
 
     @app.errorhandler(500)
     def page_error(error):
-        return 'This page error', 500
+        return render_template('error/error.html', code='500'), 500
